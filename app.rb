fb1b66d15962172862040d650d2a0283a9f3f888
Spencer Goodman <spencerwgoodman@icloud.com>
2018-06-30 21:43:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    status 200
+    "My name is Jenjamin."
+  end
+
+  get '/hometown' do
+    status 200
+    "My hometown is "
+  end
+
+  get '/favorite-song' do
+    status 200
+    "My favorite song is "
+  end
 end
